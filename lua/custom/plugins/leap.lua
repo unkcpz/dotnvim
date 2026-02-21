c62ad251398f945401d67bb1831c81c7133c08a1
Jusong Yu <jusong.d.yu@gmail.com>
2026-02-21 22:38:36 +0100

    leap move

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,5 +1,5 @@
 return {
-    'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     config = function()
         local leap = require 'leap'
         vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
