85dd696db381778b3437083f462fa3edbf14b3d7
Jusong Yu <jusong.d.yu@gmail.com>
2026-02-25 16:40:00 +0100

    rustacean: -- async-trait

--- lua/custom/plugins/rustacean.lua
+++ lua/custom/plugins/rustacean.lua
@@ -76,7 +76,7 @@ return {
                             procMacro = {
                                 enable = true,
                                 ignored = {
-                                    ['async-trait'] = { 'async_trait' },
+                                    -- ['async-trait'] = { 'async_trait' },
                                     ['napi-derive'] = { 'napi' },
                                     ['async-recursion'] = { 'async_recursion' },
                                 },
