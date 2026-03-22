e0adfc0d265447a96497f635200e06194424c27e
Jusong Yu <jusong.d.yu@gmail.com>
2026-03-22 12:54:13 +0100

    surround v3tov4

--- lua/custom/plugins/surround.lua
+++ lua/custom/plugins/surround.lua
@@ -3,19 +3,21 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-        local surround = require 'nvim-surround'
-        surround.setup {
-            keymaps = {
-                insert = '<C-g>z',
-                normal = 'gz',
-                normal_cur = 'gZ',
-                normal_line = 'gzgz',
-                normal_cur_line = 'gZgZ',
-                visual = 'gz',
-                visual_line = 'gZ',
-                delete = 'gzd',
-                change = 'gzr',
-            },
-        }
+        -- Insert mode
+        vim.keymap.set('i', '<C-g>z', '<Plug>(nvim-surround-insert)')
+
+        -- Normal mode
+        vim.keymap.set('n', 'gz', '<Plug>(nvim-surround-normal)')
+        vim.keymap.set('n', 'gZ', '<Plug>(nvim-surround-normal-cur)')
+        vim.keymap.set('n', 'gzgz', '<Plug>(nvim-surround-normal-line)')
+        vim.keymap.set('n', 'gZgZ', '<Plug>(nvim-surround-normal-cur-line)')
+
+        -- Visual mode
+        vim.keymap.set('x', 'gz', '<Plug>(nvim-surround-visual)')
+        vim.keymap.set('x', 'gZ', '<Plug>(nvim-surround-visual-line)')
+
+        -- Operators
+        vim.keymap.set('n', 'gzd', '<Plug>(nvim-surround-delete)')
+        vim.keymap.set('n', 'gzr', '<Plug>(nvim-surround-change)')
     end,
 }
